377f241a9228fcaee450886aad5b79907503d776
Reggie Davis <reginald.davis88@gmail.com>
2019-04-03 22:02:41 -0500

    Finished coding challenge

---
+++ two_fer.go
@@ -0,0 +1,14 @@
+// Package twofer should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package twofer
+
+import "fmt"
+
+// ShareWith should have a comment documenting it.
+func ShareWith(name string) string {
+	if name == "" {
+		return "One for you, one for me."
+	}
+	s := fmt.Sprintf("One for %s, one for me.", name)
+	return s
+}
